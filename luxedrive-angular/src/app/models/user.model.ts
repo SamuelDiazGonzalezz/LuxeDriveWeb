5ab54f25b0e632b6c80b1bd823b6fe59a13d4b6f
Tu Nombre <tuemail@gmail.com>
2026-04-14 17:25:38 +0100

    Actualizacion firebase y archivos firebase

--- luxedrive-angular/src/app/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface AppUser {
-  id: number | string;
-  name: string;
-  email: string;
-  password?: string;
-  role: 'admin' | 'client';
-  created_at: string;
-  last_login: string | null;
-}
