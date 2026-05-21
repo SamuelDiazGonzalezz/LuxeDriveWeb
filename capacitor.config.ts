0ff3d4da53efcc728a6b73ff301d328592e7c9a1
SamuelDiazGonzalezz <samuelsantidad4@gmail.com>
2026-05-21 19:33:08 +0100

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.luxedrive.app',
   appName: 'LuxeDrive',
-  webDir: 'dist/luxedrive-angular'
+  webDir: 'dist/luxedrive-angular/browser' //
 };
 
 export default config;
